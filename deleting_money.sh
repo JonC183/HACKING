cd4a2d8df626e8914a97557ca22afe7ba1d0e05e
Matthew Johnstone <z5255782@ad.unsw.edu.au>
2021-04-01 21:56:44 +1100

    Cool new file

--- deleting_money.sh
+++ deleting_money.sh
@@ -1,1 +1,20 @@
 echo "No more money"
+
+# Shell scripts are almost the same as command line arguments just in a file :)
+# If permission is denied, run this code on the command terminal:
+# $chmod u+x program_name 
+BOTTOM_INDEX=0
+TOP_INDEX=999
+
+
+for x in $(seq $BOTTOM_INDEX $TOP_INDEX); do
+mkdir password${x}
+
+# Clone each file
+
+done
+
+echo "Giving out credit card information..."
+echo "Unlocking passwords..."
+
+echo "Passwords successfully opened."
